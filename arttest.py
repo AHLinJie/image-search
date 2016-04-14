a75912ca892539b47422a677a7331241e0bf99fd
jielin <dev.linjie@gmail.com>
2016-04-14 08:24:38 +0800

    adjust the dir

--- arttest.py
+++ arttest.py
@@ -2,10 +2,12 @@
 
 import sys
 
+
 def main():
     print ('Hello there', "arg1:", sys.argv[1], "arg2:", sys.argv[2], "arg3", sys.argv[3], "arg0", sys.argv[0])
     # Command line args are in sys.argv[1], sys.argv[2] ..
     # sys.argv[0] is the script name itself and can be ignored
+
 
 # Standard boilerplate to call the main() function to begin
 # the program.
